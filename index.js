ec9e56ecb1bb6023f4b5023edd7f3039d1c40947
Roadagain <ringoh72@gmail.com>
2016-12-31 23:09:02 +0900

    ログは送信じゃなくて受信で出す

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var socketio = io.connect("http://localhost:8080");
 
 socketio.on("overrun", function() {
+  console.log("overrun");
   window.alert("これ以上回せません");
 });
 socketio.on("next", function(next) {
+  console.log("next");
   window.alert(next);
 });
 
 document.getElementById("next").addEventListener("click", function(ev) {
-  console.log("next");
   socketio.emit("next");
 });
 
 document.getElementById("reset").addEventListener("click", function(ev) {
-  console.log("reset");
   socketio.emit("reset");
 });
