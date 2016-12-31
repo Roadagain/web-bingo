bc58dcd1f5691b5effd22005d18a68dba1bb318d
Roadagain <ringoh72@gmail.com>
2016-12-31 23:40:58 +0900

    roller.jsのあれこれの処理をDOMContentLoadedのイベントリスナーで囲う

--- roller.js
+++ roller.js
@@ -1,16 +1,18 @@
-var socketio = io.connect("http://localhost:8080");
+document.addEventListener('DOMContentLoaded', function() {
+  var socketio = io.connect("http://localhost:8080");
 
-socketio.on("overrun", function() {
-  window.alert("これ以上回せません");
-});
-socketio.on("next", function(next) {
-  console.log(next);
-});
+  socketio.on("overrun", function() {
+    window.alert("これ以上回せません");
+  });
+  socketio.on("next", function(next) {
+    console.log(next);
+  });
 
-document.getElementById("next").addEventListener("click", function(ev) {
-  socketio.emit("next");
-});
+  document.getElementById("next").addEventListener("click", function(ev) {
+    socketio.emit("next");
+  });
 
-document.getElementById("reset").addEventListener("click", function(ev) {
-  socketio.emit("reset");
+  document.getElementById("reset").addEventListener("click", function(ev) {
+    socketio.emit("reset");
+  });
 });
