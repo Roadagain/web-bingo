da2e7f109cab6f5b57189b8b37f6a4f03361c976
Roadagain <ringoh72@gmail.com>
2016-12-31 23:09:44 +0900

    クライアントのログはイラネ

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var socketio = io.connect("http://localhost:8080");
 
 socketio.on("overrun", function() {
-  console.log("overrun");
   window.alert("これ以上回せません");
 });
 socketio.on("next", function(next) {
-  console.log("next");
   window.alert(next);
 });
 
--- result.js
+++ result.js
@@ -4,7 +4,6 @@ function setVisible(num, visible) {
 
   var id = BINGO[(num - 1) / 15 | 0] + ('0' + num).substr(-2);
   var color = COLORS[(num - 1) / 15 | 0];
-  console.log(id);
   var element = document.getElementById(id);
   if (visible){
     element.style.color = "white";
@@ -26,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
   var socketio = io.connect("http://localhost:8080");
 
   socketio.on("initialize", function(exist) {
-    console.log(exist);
     updateTable(exist);
   });
   socketio.on("next", function(next) {
